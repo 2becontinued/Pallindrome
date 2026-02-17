c72308c4903a5b00d864edbab12fe7e2f82592b3
2becontinued <smell3r@gmail.com>
2026-02-17 18:18:56 -0500

    Test 1 complete!

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,8 @@
 """
 Validates strings as palindromes.
 """
+
+
+def is_palindrome(value):  # Defines is_palindrome as a value
+    if not isinstance(value, str):
+        raise ValueError  # Raises a ValueError
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,13 @@
 """
 Tests the palindrome module
 """
+# Setup!
+
+import pytest
+from palindrome import is_palindrome
+
+#Setup end!
+
+def test_raises_a_ValueError_when_input_is_not_a_String(): # Defines a test.
+    with pytest.raises(ValueError):
+            is_palindrome(112)
