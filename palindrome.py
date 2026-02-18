8969508f7f7e6aa9412b6d7e790ca0fab26fbad9
2becontinued <smell3r@gmail.com>
2026-02-17 19:52:02 -0500

    Test 2 done.
    Imported some stuff from collections that might prove necessary later on. Completed step 2.

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,19 @@
 Validates strings as palindromes.
 """
 
+# Setup!
+
+import string
+from collections import deque
+from collections import namedtuple
+from collections import namedtuple
+
+# Setup end!
 
 def is_palindrome(value):  # Defines is_palindrome as a value
     if not isinstance(value, str):
         raise ValueError  # Raises a ValueError
+
+    if value == "":
+        return False
+
--- test_palindrome.py
+++ test_palindrome.py
@@ -6,8 +6,13 @@ Tests the palindrome module
 import pytest
 from palindrome import is_palindrome
 
+
 #Setup end!
 
 def test_raises_a_ValueError_when_input_is_not_a_String(): # Defines a test.
     with pytest.raises(ValueError):
             is_palindrome(112)
+
+def test_returns_False_when_empty_string_called():
+    assert is_palindrome("") is False
+
