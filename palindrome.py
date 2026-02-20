213ac81d16dfa6a0f9872ea05e31eca74f15b3f5
2becontinued <smell3r@gmail.com>
2026-02-19 19:55:39 -0500

    Algorithim.
    Added the  palindrome algorithim, and comments.

--- palindrome.py
+++ palindrome.py
@@ -4,9 +4,7 @@ Validates strings as palindromes.
 
 # Setup!
 
-import string
 from collections import deque
-from collections import namedtuple
 
 # Setup end!
 
@@ -14,15 +12,17 @@ def is_palindrome(value):  # Defines is_palindrome as a value
     if not isinstance(value, str):
         raise ValueError  # Raises a ValueError
 
-    if value == "":
-        return False
 
-    if value == "abc":
-        return False
+    prepared = "".join(ch.lower() for ch in value if ch.isalnum())  # Prepares the value variable for the algorithm
 
-    if value == "toronto":
-        return False
+    if prepared == "":  # If the prepared string is nothing...
+        return False  # Make it False.
 
+    d = deque(prepared)  # D is the prepared variable converted as a deque
 
+    while len(d) > 1:
 
-    return True  # IF nothing is listed that matches a input, return true.
+        if d.popleft() != d.pop():  # If it's not a palindrome...
+            return False  # Return False.
+
+    return True  # If it IS a palindrome, return True
